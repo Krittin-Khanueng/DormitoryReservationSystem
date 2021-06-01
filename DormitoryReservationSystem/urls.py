dada6b09d8f7ccbe4c2543f79bcadc4fac7d7a09
Krittin-Khanueng <6110210013@psu.ac.th>
2021-06-01 10:50:08 +0700

    startapp main

--- DormitoryReservationSystem/urls.py
+++ DormitoryReservationSystem/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	path("dorm/", include("dorm.urls"))
+	path("dorm/", include("dorm.urls")),
+	path("", include("main.urls"))
 ]
 
 if settings.DEBUG:
