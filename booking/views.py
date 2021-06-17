4875a9a5f08eba5805b698e970351e6283e37c08
Krittin Khanueng <6110210013@psu.ac.th>
2021-06-17 15:00:35 +0700

    send booking room with axois

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -10,22 +11,29 @@ from .models import Booking
 
 class BookingRoomView(View):
     def post(self, request):
-        print(request.POST.get('room_id'))
         user = self.get_user(request.user.id)
         room = self.get_room(request.POST.get('room_id'))
-        try:
-            booking = Booking(room=room, user=user)
-        except:
-            pass
-        booking.add_to_room()
-        booking.save()
-        return render(request, 'booking/booking_success.html')
-
-    def get_room(self, room_id):
-        room = Room.objects.get(room_id__exact=room_id, amount__gt=0, is_status=True)
         if room:
-            return room
-        return None
+            try:
+                booking = Booking(room=room, user=user)
+            except:
+                pass
+            booking.add_to_room()
+            booking.save()
+            # return render(request, 'booking/booking_success.html')
+            return JsonResponse({"status": 'Success'})
+        else:
+            return JsonResponse({"status": 'ห้องเต็มแล้ว'})
+
+
+    # get room
+    def get_room(self, room_id):
+
+        try:
+            room = Room.objects.get(room_id__exact=room_id, amount__gt=0, is_status=True)
+        except Room.DoesNotExist:
+            room = None
+        return room
 
     def get_user(self, user_id):
         user = get_object_or_404(User, id=user_id)
