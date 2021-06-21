0e8504ba4186098aaeb17a5e8c3f0a566cf5d23e
Krittin Khaneung <6110210013@psu.ac.th>
2021-06-21 21:31:05 +0700

    update model booking

--- booking/models.py
+++ booking/models.py
@@ -8,11 +8,11 @@ from dorm.models import Room
 class Booking(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True, related_name='booking_room')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, )
+    user = models.ManyToManyField(User)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"ชื่อ:{self.user} {self.room}"
+        return f"{self.room}"
 
     def add_to_room(self):
         self.room.amount -= 1
