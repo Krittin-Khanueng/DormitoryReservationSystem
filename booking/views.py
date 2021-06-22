5922914066d55a39a06ba5c1d1adfc0b9d22c713
Krittin Khaneung <6110210013@psu.ac.th>
2021-06-22 16:10:39 +0700

    update booking confirm

--- booking/views.py
+++ booking/views.py
@@ -10,32 +10,38 @@ from .models import Booking
 
 
 class BookingRoomView(View):
-    def post(self, request):
-        print(request.POST.get('room_id'))
-        user = self.get_user(request.user.id)
-        room = self.get_room(request.POST.get('room_id'))
-        try:
-            booking = Booking(room=room, user=user)
-        except:
-            pass
-        booking.add_to_room()
-        booking.save()
-        return render(request, 'booking/booking_success.html')
+	def post(self, request):
+		print(request.POST.get('room_id'))
+		user = self.get_user(request.user.id)
+		room = self.get_room(request.POST.get('room_id'))
+		try:
+			booking = Booking(room=room, user=user)
+		except:
+			pass
+		booking.add_to_room()
+		booking.save()
+		return render(request, 'booking/booking_success.html')
 
-    def get_room(self, room_id):
-        room = Room.objects.get(room_id__exact=room_id, amount__gt=0, is_status=True)
-        if room:
-            return room
-        return None
+	def get_room(self, room_id):
+		room = Room.objects.get(room_id__exact=room_id, amount__gt=0, is_status=True)
+		if room:
+			return room
+		return None
 
-    def get_user(self, user_id):
-        user = get_object_or_404(User, id=user_id)
-        if user:
-            return user
-        return None
+	def get_user(self, user_id):
+		user = get_object_or_404(User, id=user_id)
+		if user:
+			return user
+		return None
 
 
 class ConfirmRoomView(View):
-    def post(self, request):
-        room_id = request.POST.get('room_id')
-        return render(request, 'booking/booking_confirm.html')
+	def post(self, request):
+		user = User.objects.get(id=request.user.id)
+		room = Room.objects.get(id=request.POST.get('room_id'))
+		booking = get_object_or_404(Booking, user=user, room=room)
+		context = {
+			"room": room,
+			"booking": booking
+		}
+		return render(request, 'booking/booking_confirm.html', context)
