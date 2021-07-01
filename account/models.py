c524bc84f42b1a5a4144c88fe451725d5e1c03f3
Krittin Khaneung <6110210013@psu.ac.th>
2021-07-01 18:54:17 +0700

    crate models dorm account

--- account/models.py
+++ account/models.py
@@ -6,17 +6,20 @@ from django.db import models
 
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField('เบอร์โทรศัพท์', max_length=10, null=True, blank=True)
-    gender = models.CharField('เพศ', max_length=7, choices=[('ผู้ชาย', 'ผู้ชาย'), ('ผู้หญิง', 'ผู้หญิง')], null=True,
+    phone_number = models.CharField(verbose_name='เบอร์โทรศัพท์', max_length=10, null=True, blank=True)
+    bank_account = models.CharField(verbose_name='บัญชีธนาคาร', max_length=20, null=True, blank=True)
+    motorcycle_registration = models.CharField(verbose_name="ป้ายทะเบียนรถจักรยานยนต์", max_length=15, null=True,
+                                               blank=True)
+    car_registration = models.CharField(verbose_name="ป้ายทะเบียนยนต์", max_length=15, null=True, blank=True)
+    gender = models.CharField(verbose_name='เพศ', max_length=7, choices=[('ผู้ชาย', 'ผู้ชาย'), ('ผู้หญิง', 'ผู้หญิง')],
+                              null=True,
                               blank=True)
     image = models.ImageField(upload_to="students/", blank=True)
-    current_state = models.CharField('สถานะ', max_length=14,
+    current_state = models.CharField(verbose_name='สถานะ', max_length=14,
                                      choices=[('ไม่มีสิทธิ์จอง', 'ไม่มีสิทธิ์จอง'), ('มีสิทธิ์จอง', 'มีสิทธิ์จอง'),
                                               ('รอยืนยัน', 'รอยืนยัน'), ('ยืนยัน', 'ยืนยัน')], default='ไม่มีสิทธิ์จอง')
 
+    room_number = models.CharField(verbose_name="เลขห้อง", max_length=10, null=True, blank=True)
+
     def __str__(self):
         return self.user.username
-
-
-class Group(models.Model):
-    pass
--- dorm/models.py
+++ dorm/models.py
@@ -5,35 +5,36 @@ from django.db import models
 
 
 class Dormitory(models.Model):
-	name = models.CharField(max_length=100, unique=True)
-	description = models.TextField(blank=True)
-	images = models.ImageField(upload_to="images/dorm/", blank=True, null=True)
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    images = models.ImageField(upload_to="images/dorm/", blank=True, null=True)
+    images_room_plan = models.ImageField(upload_to="images/dorm/room_plan/", blank=True, null=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	def room_not_full(self):
-		return Floor.objects.filter(dorm_name=self, floor__amount__gt=0).count()
+    def room_not_full(self):
+        return Floor.objects.filter(dorm_name=self, floor__amount__gt=0).count()
 
 
 class Floor(models.Model):
-	dorm_name = models.ForeignKey(Dormitory, on_delete=models.PROTECT, related_name='dormitory',
-								  related_query_name="dormitory",
-								  verbose_name="ชื่อหอพัก")
-	number = models.PositiveIntegerField(default=1, verbose_name="ชั้น")
+    dorm_name = models.ForeignKey(Dormitory, on_delete=models.PROTECT, related_name='dormitory',
+                                  related_query_name="dormitory",
+                                  verbose_name="ชื่อหอพัก")
+    number = models.PositiveIntegerField(default=1, verbose_name="ชั้น")
 
-	def __str__(self):
-		return f"หอพัก:{self.dorm_name.name} ชั้น:{self.number}"
+    def __str__(self):
+        return f"หอพัก:{self.dorm_name.name} ชั้น:{self.number}"
+
 
 class Room(models.Model):
-	TYPE_IN_ROOM = [('MALE', 'ผู้ชาย'), ('FEMALE', 'ผู้หญิง')]
-	floor = models.ForeignKey(Floor, on_delete=models.PROTECT, related_name="floor", related_query_name="floor",
-							  verbose_name="ชั้น")
-	room_id = models.CharField(max_length=10, blank=True, null=True, unique=True, verbose_name="เลขห้อง")
-	room_type = models.CharField(max_length=10, choices=TYPE_IN_ROOM, verbose_name="ประเภทห้องพัก")
-	amount = models.PositiveIntegerField(default=3, verbose_name="จำนวน")
-	is_status = models.BooleanField(default=True, verbose_name="สถานะของห้อง")
+    TYPE_IN_ROOM = [('MALE', 'ผู้ชาย'), ('FEMALE', 'ผู้หญิง')]
+    floor = models.ForeignKey(Floor, on_delete=models.PROTECT, related_name="floor", related_query_name="floor",
+                              verbose_name="ชั้น")
+    room_id = models.CharField(max_length=10, blank=True, null=True, unique=True, verbose_name="เลขห้อง")
+    room_type = models.CharField(max_length=10, choices=TYPE_IN_ROOM, verbose_name="ประเภทห้องพัก")
+    amount = models.PositiveIntegerField(default=3, verbose_name="จำนวน")
+    is_status = models.BooleanField(default=True, verbose_name="สถานะของห้อง")
 
-	def __str__(self):
-		return f"หอพัก:{self.floor.dorm_name} ชั้น:{self.floor.number} ห้อง:{self.room_id} จำนวน:{self.amount}"
-
+    def __str__(self):
+        return f"หอพัก:{self.floor.dorm_name} ชั้น:{self.floor.number} ห้อง:{self.room_id} จำนวน:{self.amount}"
