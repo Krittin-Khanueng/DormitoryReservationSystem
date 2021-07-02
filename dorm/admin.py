39a6e2c37e943a5b9f3876c1b7c1ba11585d71f3
Krittin Khaneung <6110210013@psu.ac.th>
2021-07-02 10:17:07 +0700

    update admin dorm

--- dorm/admin.py
+++ dorm/admin.py
@@ -4,10 +4,18 @@ from .models import Dormitory, Room, Floor
 
 # Register your models here.
 
+class DormitoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'is_active')
+
+
+class FloorAdmin(admin.ModelAdmin):
+    list_display = ('dorm_name', 'number')
+
+
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('floor', 'room_id', 'room_type', 'amount', 'is_status')
 
 
-admin.site.register(Dormitory)
+admin.site.register(Dormitory, DormitoryAdmin)
 admin.site.register(Room, RoomAdmin)
-admin.site.register(Floor)
+admin.site.register(Floor, FloorAdmin)
--- dorm/views.py
+++ dorm/views.py
@@ -1,29 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
-
 
 from .models import Dormitory, Room
 
 
 class DormView(LoginRequiredMixin, View):
-	login_url = "login"
+    login_url = "login"
 
-	def get(self, request):
-		dorm = Dormitory.objects.all()
-		context = {
-			"dormitorys": dorm,
+    def get(self, request):
+        dorm = Dormitory.objects.filter(is_active=True)
+        context = {
+            "dormitorys": dorm,
 
-		}
-		return render(request, 'dorm/dorm.html', context)
+        }
+        return render(request, 'dorm/dorm.html', context)
 
 
 class RoomView(LoginRequiredMixin, View):
-	login_url = "login"
+    login_url = "login"
 
-	def get(self, request, pk):
-		context = {}
-		dorm = Dormitory.objects.get(pk=pk)
-		context["floors"] = dorm.dormitory.all()
-		return render(request, "dorm/room.html", context)
-
+    def get(self, request, pk):
+        context = {}
+        dorm = get_object_or_404(Dormitory, pk=pk, is_active=True)
+        context["floors"] = dorm.dormitory.all()
+        return render(request, "dorm/room.html", context)
