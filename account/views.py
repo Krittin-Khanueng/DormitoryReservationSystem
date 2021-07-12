d5bb9f2d3a31e01c02b9e0770f11dcb05f3b42cf
Krittin Khaneung <6110210013@psu.ac.th>
2021-07-12 21:10:53 +0700

    add url profile in navbar

--- account/views.py
+++ account/views.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.shortcuts import render
 
 
 class LoginView(LoginView):
@@ -7,3 +10,12 @@ class LoginView(LoginView):
 
 class LogoutView(LogoutView):
     next_page = 'index'
+
+
+class ProfileView(LoginRequiredMixin, View):
+    login_url = "login"
+    def get(self, request):
+        #get request user and account
+        user = request.user
+        account = user.account
+        return render(request, 'account/profile.html', {'account':account})
