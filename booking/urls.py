d1accb8b4ab91b18fd52a9b38b61ff8e979e4f55
Krittin Khaneung <6110210013@psu.ac.th>
2021-07-26 15:37:13 +0700

    update confirm

--- booking/urls.py
+++ booking/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 	path("confirm/", views.ConfirmRoomView.as_view(), name="confirm"),
 	path("success/", views.BookingSuccessView.as_view(), name="booking_success"),
 	path("confirm/booking/", views.ConfirmToBookView.as_view(), name="booking_confirm"),
+	# path("history/",)
 	# path("cancel/",),
 	# path("cancel-succes/"),
 
