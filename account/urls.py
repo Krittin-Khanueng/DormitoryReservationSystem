1e420684992041bf82d5cb2b607442244351476e
Krittin Khanueng <6110210013@psu.ac.th>
2021-08-11 15:13:28 +0700

    dele edit profile

--- account/urls.py
+++ account/urls.py
@@ -7,7 +7,6 @@ from . import views
 
 urlpatterns = [
 	path('profile/',views.ProfileView.as_view(),name='profile'),
-	path('profile/edit/',views.ProfileEditView.as_view(),name='profile_edit'),
 	path('login/', views.LoginView.as_view(), name="login"),
 	path('logout/', views.LogoutView.as_view(), name="logout"),
 	
