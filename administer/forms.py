9bb0a6474934fbb3b88ab58abe1d0d9cc7dd03e0
Krittin Khaneung <6110210013@psu.ac.th>
2021-08-29 22:18:02 +0700

    update forms and show all dorm

---
+++ administer/forms.py
@@ -0,0 +1,23 @@
+﻿from django import forms
+from dorm.models import Dormitory
+
+
+class DormitoryForm(forms.ModelForm):
+    class Meta:
+        model = Dormitory
+        fields = ('name', 'images', 'images_room_plan')
+
+    def __init__(self, *args, **kwargs):
+        super(DormitoryForm, self).__init__(*args, **kwargs)
+        self.fields['images_room_plan'].widget.attrs.update(
+            {'class': 'form-control'})
+        self.fields['name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['images'].widget.attrs.update({'class': 'form-control'})
+        # label
+        self.fields['name'].label = 'ชื่อหอพัก'
+        self.fields['images'].label = 'รูปหอพัก'
+        self.fields['images_room_plan'].label = 'รูปชั้นของหอพัก'
+
+        # required fields
+        self.fields['name'].required = True
+        self.fields['images'].required = True
--- administer/views.py
+++ administer/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
-# Create your views here.
+
+from .forms import DormitoryForm
+from dorm.models import Dormitory
+
+from django.views import View
+from django.contrib import messages
 
 
-class administerView(TemplateView):
-    template_name = "administer/base_admin_page.html"
+class administerView(View):
+    def get(self, request):
+        return render(request, "administer/base_admin_page.html")
 
 
-class administerdashboardView(TemplateView):
-    template_name = "administer/admin_dashboard.html"
+class administerdashboardView(View):
+    def get(self, request):
+        return render(request, "administer/dashboard.html")
 
 
-class administerdormView(TemplateView):
-    template_name = "administer/admin_dorm_management.html"
+class administerdormView(View):
+    def get(self, request):
+
+        context = {
+            "form": DormitoryForm(),
+            "dorms": Dormitory.objects.all()
+        }
+        return render(request, "administer/dorm_management.html", context)
+
+    def post(self, request):
+        form = DormitoryForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "บันทึกสำเร็จ")
+            return render(request, "administer/dorm_management.html")
+        else:
+            context = {
+                "form": form
+            }
+            return render(request, "administer/dorm_management.html", context)
