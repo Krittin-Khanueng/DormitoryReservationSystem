738066ef91239f2f29f6fe5633cf569a1976e54b
Krittin Khaneung <6110210013@psu.ac.th>
2021-08-29 22:41:41 +0700

    add pagination in dorm management

--- administer/views.py
+++ administer/views.py
@@ -5,6 +5,7 @@ from dorm.models import Dormitory
 
 from django.views import View
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 
 class administerView(View):
@@ -19,10 +20,23 @@ class administerdashboardView(View):
 
 class administerdormView(View):
     def get(self, request):
+        dorms = Dormitory.objects.all()
+        paginator = Paginator(dorms, 3)
+
+        try:
+            page = int(request.GET.get('page', '1'))
+        except:
+            page = 1
+
+        try:
+            dormPage = paginator.page(page)
+        except (EmptyPage, InvalidPage):
+            dormPage = paginator.page(paginator.num_pages)
 
         context = {
             "form": DormitoryForm(),
-            "dorms": Dormitory.objects.all()
+            "dorms": dormPage,
+
         }
         return render(request, "administer/dorm_management.html", context)
 
