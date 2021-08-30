2554c1e6fd5a6b5ff9fda683e18f44380af8af01
Krittin Khaneung <6110210013@psu.ac.th>
2021-08-30 21:28:05 +0700

    Add manage floors and rooms.

--- administer/forms.py
+++ administer/forms.py
@@ -1,5 +1,5 @@
 ﻿from django import forms
-from dorm.models import Dormitory
+from dorm.models import Dormitory, Floor, Room
 
 
 class DormitoryForm(forms.ModelForm):
@@ -21,3 +21,43 @@ class DormitoryForm(forms.ModelForm):
         # required fields
         self.fields['name'].required = True
         self.fields['images'].required = True
+
+
+class FloorForm(forms.ModelForm):
+    class Meta:
+        model = Floor
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(FloorForm, self).__init__(*args, **kwargs)
+        self.fields['dorm_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['number'].widget.attrs.update({'class': 'form-control'})
+        self.fields['dorm_name'].label = 'ชื่อหอพัก'
+        self.fields['number'].label = 'เลขชั้น'
+
+        # required fields
+        self.fields['dorm_name'].required = True
+        self.fields['number'].required = True
+
+
+class RoomForm(forms.ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(RoomForm, self).__init__(*args, **kwargs)
+        self.fields['floor'].widget.attrs.update({'class': 'form-control'})
+        self.fields['room_id'].widget.attrs.update({'class': 'form-control'})
+
+        self.fields['room_type'].widget.attrs.update({'class': 'form-control'})
+        self.fields['amount'].widget.attrs.update({'class': 'form-control'})
+        # is_status boolen fields
+
+
+        # label
+        self.fields['floor'].label = 'ชั้น'
+        self.fields['room_id'].label = 'เลขห้อง'
+        self.fields['room_type'].label = 'ประเภทห้อง'
+        self.fields['amount'].label = 'จำนวนคน'
+        self.fields['is_status'].label = 'สถานะ'
--- administer/views.py
+++ administer/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from .forms import DormitoryForm
-from dorm.models import Dormitory
+from .forms import DormitoryForm, FloorForm, RoomForm
+from dorm.models import Dormitory, Floor, Room
 
 from django.views import View
 from django.contrib import messages
@@ -20,7 +20,7 @@ class administerdashboardView(View):
         return render(request, "administer/dashboard.html")
 
 
-class administerdormView(View):
+class administerdorm_addView(View):
     def get(self, request):
         dorms = Dormitory.objects.all()
         paginator = Paginator(dorms, 3)
@@ -40,19 +40,19 @@ class administerdormView(View):
             "dorms": dormPage,
 
         }
-        return render(request, "administer/dorm_management.html", context)
+        return render(request, "administer/dorm/dorm_management_add.html", context)
 
     def post(self, request):
         form = DormitoryForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             messages.success(request, "บันทึกสำเร็จ")
-            return HttpResponseRedirect(reverse("management_dorm"))
+            return HttpResponseRedirect(reverse("management_dorm_add"))
         else:
             context = {
                 "form": form
             }
-            return render(request, "administer/dorm_management.html", context)
+            return render(request, "administer/dorm/dorm_management_add.html", context)
 
 
 class administerdorm_editView(View):
@@ -62,7 +62,7 @@ class administerdorm_editView(View):
             "form": DormitoryForm(instance=dorm),
             "dorm": dorm
         }
-        return render(request, "administer/dorm_management_edit.html", context)
+        return render(request, "administer/dorm/dorm_management_edit.html", context)
 
     def post(self, request, id):
         dorm = Dormitory.objects.get(id=id)
@@ -70,12 +70,12 @@ class administerdorm_editView(View):
         if form.is_valid():
             form.save()
             messages.success(request, "บันทึกสำเร็จ")
-            return HttpResponseRedirect(reverse("management_dorm"))
+            return HttpResponseRedirect(reverse("management_dorm_add"))
         else:
             context = {
                 "form": form
             }
-            return render(request, "administer/dorm_management_edit.html", context)
+            return render(request, "administer/dorm/dorm_management_edit.html", context)
 
 
 class administerdorm_deleteView(View):
@@ -83,4 +83,93 @@ class administerdorm_deleteView(View):
         id = request.POST.get("id")
         dorm = Dormitory.objects.get(id=id)
         dorm.delete()
+        return JsonResponse({"status": 'success'})
+
+
+class administerdormView(View):
+    def get(self, request):
+        return render(request, "administer/dorm/dorm_management.html")
+
+
+class administerfloor_addView(View):
+    def get(self, request):
+        context = {
+            "form": FloorForm(),
+        }
+        return render(request, "administer/dorm/floor/floor_management_add.html", context)
+
+    def post(self, request):
+        form = FloorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "บันทึกสำเร็จ")
+            return HttpResponseRedirect(reverse("management_floor_add"))
+        else:
+            context = {
+                "form": form
+            }
+            return render(request, "administer/dorm/floor/floor_management_add.html", context)
+
+
+class administerroom_addView(View):
+    def get(self, request):
+        rooms = Room.objects.all()
+        paginator = Paginator(rooms, 3)
+
+        try:
+            page = int(request.GET.get('page', '1'))
+        except:
+            page = 1
+
+        try:
+            roomPage = paginator.page(page)
+        except (EmptyPage, InvalidPage):
+            roomPage = paginator.page(paginator.num_pages)
+
+        context = {
+            "form": RoomForm(),
+            "rooms": roomPage
+        }
+        return render(request, "administer/dorm/room/room_management_add.html", context)
+
+    def post(self, request):
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "บันทึกสำเร็จ")
+            return HttpResponseRedirect(reverse("management_room_add"))
+        else:
+            context = {
+                "form": form
+            }
+            return render(request, "administer/dorm/room/room_management_add.html", context)
+
+
+class administerroom_editView(View):
+    def get(self, request, id):
+        room = Room.objects.get(id=id)
+        context = {
+            "form": RoomForm(instance=room),
+        }
+        return render(request, "administer/dorm/room/room_management_edit.html", context)
+
+    def post(self, request, id):
+        room = Room.objects.get(id=id)
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "บันทึกสำเร็จ")
+            return HttpResponseRedirect(reverse("management_room_add"))
+        else:
+            context = {
+                "form": form
+            }
+            return render(request, "administer/dorm/room/room_management_edit.html", context)
+
+
+class administerroom_deleteView(View):
+    def post(self, request):
+        id = request.POST.get("id")
+        room = Room.objects.get(id=id)
+        room.delete()
         return JsonResponse({"status": 'success'})
